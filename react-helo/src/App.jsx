9a27b7551127a6eb0e1ca41eac68e01e0b027ac6
akshaykumark1 <kumarakshay90574@gmail.com>
2025-04-08 16:17:53 +0530

    done

---
+++ react-helo/src/App.jsx
@@ -0,0 +1,103 @@
+import  React from 'react'
+
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <>
+//       <h1>helo</h1>
+//       </>
+//     )
+//   }
+// }
+
+// state #######################################//
+// class App extends React.Component{
+//   constructor(){
+//     super()
+//     this.state={name:"akshay"}
+//   }
+//   render(){
+//     return(
+//       <>
+//       <h1>hello {this.state.name}</h1>
+//       </>
+//     )
+//   }
+// }
+// ##############################################################
+
+// ############PROBS############DATA PASSING METHOD#####33
+// class App extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state={name:props.name,age:props.age}
+//   }
+//   render(){
+//     return(
+//       <>
+//       <h1>hello {this.state.name}</h1>
+//       <h1>age {this.state.age}</h1>
+//       </>
+//     )
+//   }
+// }
+
+
+
+
+// nested class components##########################
+
+// class Demo extends React.Component{
+//   render(){
+//     return (
+//       <>
+//       <h1>Demo components {this.props.name} </h1>
+//       </>
+//     )
+//   }
+// }
+
+
+
+
+// class App extends React.Component {
+//   render(){
+//     return(
+//       <>
+//       <h1>hello</h1>
+//       <h2>age {this.props.age}</h2>
+//       <Demo name = {this.props.age}/>
+//       </>
+//     )
+//   }
+
+// }
+
+// implementing user defined function#######################################
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { age: 20 };
+  }
+
+  changeAge = () => {
+    // Use setState with the correct capitalization
+    this.setState({ age: 25 });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>hello</h1>
+        <h2>age {this.state.age}</h2>
+        <button onClick={this.changeAge}>Change Age</button>
+      </>
+    );
+  }
+}
+
+
+
+
+export default App
